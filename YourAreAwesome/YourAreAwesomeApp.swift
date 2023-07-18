b421ec6377596bf354c7460fa70d4b218551ec8c
Alex Labanino <ghostrider6610@gmail.com>
2023-07-18 19:26:10 -0400

    Initial Commit

---
+++ YourAreAwesome/YourAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourAreAwesomeApp.swift
+//  YourAreAwesome
+//
+//  Created by Alex Labanino on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct YourAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
